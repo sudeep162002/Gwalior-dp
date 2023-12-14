784788edf1d659cecf0df05abc8b8cc050944700
sudeep162002 <80563363+sudeep162002@users.noreply.github.com>
2023-12-14 21:08:31 +0530

    removed-commented-garbage-code

--- src/index.ts
+++ src/index.ts
@@ -26,68 +26,6 @@ DocRouts(app);
 UserRouts(app);
 
 
-// app.post('/insert', async (req,res)=>{
-//   const user: User = req.body;
-
-//   console.log(req.body)
-//   if (!req.body || !req.body.fullName) {
-//     return res.status(400).json({ message: 'Invalid request body.' });
-//   }
-//   const existingUser = await Users.findOne({ fullName: user.fullName });
-//     if (existingUser) {
-//       return res.status(400).json({ message: 'User already exists.' });
-//     }
-
-//   const newUser = new Users({
-//     ...user,
-//   });
-//   try {
-//     const result = await newUser.save();
-//     res.status(200).json({ message: 'Data successfully inserted.', result });
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error inserting data.', error: error });
-//   }
-  
-// })
-
-
-// app.put('/update/:id', async (req,res)=>{
-//   const user: User = req.body;
-
-//   if (!req.body || !req.body.fullName) {
-//     return res.status(400).json({ message: 'Invalid request body.' });
-//   }
-//   const existingUser = await Users.findOne({ id: user._id });
-//     if (existingUser) {
-//       return res.status(400).json({ message: 'User already exists.' });
-//     }
-
-//   const newUser = new Users({
-//     ...user,
-//   });
-//   try {
-//     const result = await newUser.save();
-//     res.status(200).json({ message: 'Data successfully inserted.', result });
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error inserting data.', error: error });
-//   }
-  
-// })
-
-// app.get('/get-users', async (req,res)=>{
-//   try{
-//     // const usersList= await Users.findMany();
-//     const usersList = await Users.find();
-//     if(!usersList || usersList.length==0) res.status(400).json({message: 'No usrs find in databse. '})
-
-//     res.status(200).json(usersList);
-//   }catch(err){
-//     res.status(500).json({error: err});
-//   }
-// })
-
-
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });

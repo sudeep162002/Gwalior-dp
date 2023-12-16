25e865c0460b84809bbcd83ad25a2d820ca720b8
sudeep162002 <80563363+sudeep162002@users.noreply.github.com>
2023-12-16 17:03:14 +0530

    fix-auth

--- src/application/middleware/auth.ts
+++ src/application/middleware/auth.ts
@@ -3,11 +3,14 @@ import * as dotenv from 'dotenv';
 // personal auth for one person only
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('Authorization')?.split(' ')[1];
+    const token = req.header('Authorization');
     try {
+        
         if (!token || token!=process.env.PRIVATE_TOKEN) {
+         
         return res.status(401).json({ message: 'Unauthorized' });
         }
+        
         next();
     } catch (error) {
       return res.status(403).json({ message: 'Forbidden' });
